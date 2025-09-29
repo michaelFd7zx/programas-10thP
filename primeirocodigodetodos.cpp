24e63e765255a93d9848e1c12940c46ee7ece2bc
michaelFd7zx <michaelgamesdahorak@gmail.com>
2025-09-29 11:43:14 +0100

    Add files via upload

---
+++ primeirocodigodetodos.cpp
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"se a tua mãe tivesse um relógio no braço esquerdo, e outro no braço direito daria fuso horário.😊";
+
+    return 0;
+}
