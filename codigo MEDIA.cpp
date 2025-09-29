651037b49af43043e1cf9324c62a4158922d55b8
michaelFd7zx <michaelgamesdahorak@gmail.com>
2025-09-29 12:27:16 +0100

    Add files via upload

---
+++ codigo MEDIA.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main() {
+    int p1, p2, p3, p4, media;
+    
+    std::cout << "por favor escreva um número.";
+    std::cin >> p1;
+     std::cout << "por favor escreva um número.";
+     std::cin >> p2;
+      std::cout << "por favor escreva um número.";
+      std::cin >> p3;
+       std::cout << "por favor escreva um número.";
+       std::cin >> p4;
+       
+       media = (p1+p2+p3+p4)/4;
+       
+      std::cout << "A média dos números é " << media << ".";}
+
+
